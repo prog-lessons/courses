064b6e2b7d62d179522c5f9df7e87788367dd467
Carlos Eduardo Olivieri <caduolivieri1984@gmail.com>
2020-03-27 20:34:01 -0300

    Add files via upload

---
+++ Coursera-Algorithmic-Toolbox/week1_programming_challenges/APlusB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class APlusB
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+        s.close();
+    }
+}
